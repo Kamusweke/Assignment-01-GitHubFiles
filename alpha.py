5298e80983d71e73a622a6196e4632618598fc7a
knox <knox.chiza@gmail.com>
2019-04-13 11:38:22 +0000

    I am pushing all my assignment files to my GitHub

---
+++ alpha.py
@@ -0,0 +1,1 @@
+print("Hello, World!This is my python program for assgnment-01")
---
+++ sigma.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main(){
+
+printf(“Hello! I am C-Program for assigment01. Thank you!”);
+
+return 0;
+
+}
+
+
